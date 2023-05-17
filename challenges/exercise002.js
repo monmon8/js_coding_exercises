d0312098ae9fa7468230fcb98d1e62874877ebf5
Ahmed <imaneben8@gmail.com>
2023-05-17 23:26:06 +0100

    feat:added getBusNumber functionality

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -17,6 +17,15 @@ export function isFromManchester(person) {
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  if (people <= 40) {
+    return 1;
+  } else if (people <= 80 && people > 40) {
+    return 2;
+  } else if (people <= 120 && people > 80) {
+    return 3;
+  } else {
+    return Math.ceil(people / 40);
+  }
 }
 
 export function countSheep(arr) {
