23cbd16a53839635246e90b0a724ba583acd0fe2
Ahmed <imaneben8@gmail.com>
2023-05-20 23:17:43 +0100

    feat:added findNamesBeginning  functionality

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -12,7 +12,13 @@ export function findSmallNums(nums) {
 export function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let newArr = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].charAt(0) === char) {
+      newArr.push(names[i]);
+    }
+  }
+  return newArr;
 }
 
 export function findVerbs(words) {
