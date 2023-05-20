48a6ad5644a8d8aaf7e1fbe59a96f4e4d67b673b
Ahmed <imaneben8@gmail.com>
2023-05-20 23:58:18 +0100

    feat:added getCities functionality

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -47,7 +47,7 @@ export function getIntegers(nums) {
 
 export function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  return users.map((user) => user.data.city.displayName);
 }
 
 export function getSquareRoots(nums) {
