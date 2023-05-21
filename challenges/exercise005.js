ad63413d451a4fbd6df7cbb86b85611976aad496
Ahmed <imaneben8@gmail.com>
2023-05-21 13:04:45 +0100

    feat:adde sumArrays functionality

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -33,7 +33,14 @@ export const reverseNumber = (n) => {
 
 export const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  let totalSum = 0;
+  for (let i = 0; i < arrs.length; i++) {
+    const subArray = arrs[i];
+    for (let j = 0; j < subArray.length; j++) {
+      totalSum += subArray[j];
+    }
+  }
+  return totalSum;
 };
 
 export const arrShift = (arr) => {
